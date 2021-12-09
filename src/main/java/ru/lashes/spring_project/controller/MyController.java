4eee3830ec619fd1fab66e510e4324a18ca0b39f
SolomonVP <Lum7023@yandex.ru>
2021-12-09 18:21:40 +0300

    - add service. Refactor @Transaction

--- src/main/java/ru/lashes/spring_project/controller/MyController.java
+++ src/main/java/ru/lashes/spring_project/controller/MyController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.lashes.spring_project.dao.ClientDAO;
 import ru.lashes.spring_project.entity.Client;
+import ru.lashes.spring_project.service.ClientService;
 
 import java.util.List;
 
@@ -13,11 +14,11 @@ import java.util.List;
 public class MyController {
 
     @Autowired
-    private ClientDAO clientDAO;
+    private ClientService clientService;
 
     @RequestMapping("/")
     public String showAllClients(Model model) {
-        List<Client> allClients = clientDAO.getAllClients();
+        List<Client> allClients = clientService.getAllClients();
         model.addAttribute("allClnts", allClients);
         return "all-clients";
     }
